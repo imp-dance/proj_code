159602c66cd7cafc4a3585d0d89cd22d44fdd5f2
schart <ha>
2014-04-11 16:05:36 +0200

    hmmmmf

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
     $("#nav").load("../demo/nav.html"); /* Load nav */
     $(".sidebar").load("../demo/sidebar.html"); /* Load sidebar */
-	$("a").hover(function(){
-		if ($(this).attr("target")){
-			$("#coderun").text("cd (" + $(this).attr("href") + ", " + $(this).attr("target") + ")");
-		}else{
-			$("#coderun").text("cd " + $(this).attr("href"));
-		}
-	}, function(){
-		$("#coderun").text("");
-	});
+});
+$("a").hover(function(){
+    if ($(this).attr("target")){
+        $("#coderun").text("cd (" + $(this).attr("href") + ", " + $(this).attr("target") + ")");
+    }else{
+        $("#coderun").text("cd " + $(this).attr("href"));
+    }
+}, function(){
+    $("#coderun").text("");
 });
