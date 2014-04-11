06b142fc78812380716b968a5b13331e9b818ff1
schart <ha>
2014-04-11 16:13:11 +0200

    .live

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,20 @@ $(document).ready(function(){
     $("#nav").load("../demo/nav.html"); /* Load nav */
     $(".sidebar").load("../demo/sidebar.html"); /* Load sidebar */
 });
+/*
 $("a").live('hover', function(){
-    if ($(this).attr("target")){
-        $("#coderun").text("cd (" + $(this).attr("href") + ", " + $(this).attr("target") + ")");
-    }else{
-        $("#coderun").text("cd " + $(this).attr("href"));
-    }
 }, function(){
-    $("#coderun").text("");
+});
+*/
+$("a").live({
+    mouseenter: function () {
+        if ($(this).attr("target")){
+            $("#coderun").text("cd (" + $(this).attr("href") + ", " + $(this).attr("target") + ")");
+        }else{
+            $("#coderun").text("cd " + $(this).attr("href"));
+        }
+    },
+    mouseleave: function () {
+        $("#coderun").text("");
+    }
 });
