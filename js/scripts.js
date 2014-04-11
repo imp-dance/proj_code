231146a537b7dd8d865c07ba12343a80fe99b3ef
schart <ha>
2014-04-11 16:26:25 +0200

    ffs

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,17 @@
 $(document).ready(function(){
     $("#nav").load("../demo/nav.html", function(){
         $(".sidebar").load("../demo/sidebar.html", function(){
-               $("a").hover(function(){
-                    if ($(this).attr("target")){
-                        $("#coderun").text("cd (" + $(this).attr("href") + ", " + $(this).attr("target") + ")");
-                    }else{
-                        $("#coderun").text("cd " + $(this).attr("href"));
-                    }
-                }, function(){
-                    $("#coderun").text("");
-                });
-            });
+             $("a").hover(function(){
+                if ($(this).attr("target")){
+                    $("#coderun").text("cd (" + $(this).attr("href") + ", " + $(this).attr("target") + ")");
+                }else{
+                    $("#coderun").text("cd " + $(this).attr("href"));
+                }
+            }, function(){
+                $("#coderun").text("");
+            }); /* Hover */
         }); /* Load sidebar */
-        
-    }); /* Load nav */
-    
+    }); /* Load Nav */
 });
 /*
 $("a").live('hover', function(){
